79397ea0b6b8ccd8fa138d667823824fbc985175
Samuel Campos <samuel.campos@feedzai.com>
2017-05-11 15:32:39 +0100

    Change java aliases in order to not compile the unit tests when performing fast builds.

--- rc-files/java.sh
+++ rc-files/java.sh
@@ -1,12 +1,15 @@
 # Export environment variables
 export JAVA_HOME=$(/usr/libexec/java_home)
-#export JAVA_HOME=$(/usr/libexec/java_home -v 1.8.0_111)
 
 # Maven aliases & tweeks
 # - https://zeroturnaround.com/rebellabs/your-maven-build-is-slow-speed-it-up/
 # - http://blog.dblazejewski.com/2015/08/how-to-make-your-maven-build-fast-again/
 export MAVEN_OPTS="-XX:+TieredCompilation -XX:TieredStopAtLevel=1"
-alias mdu="mvn versions:display-dependency-updates" # maven dependency updates
-alias mist="mvn -T 1C install -DskipTests=true"     # maven install skip tests
-alias mfi="mist -nsu -o"                            # maven install skip tests offline
-alias mcfi="mvn -T 1C clean install -DskipTests=true -nsu -o" # maven clean install skip tests offline
+
+# -DskipTests=true          : Don't run the unit tests
+# -Dmaven.test.skip=true    : Don't compile nor run the unit tests
+
+alias mdu="mvn versions:display-dependency-updates"                     # maven dependency updates
+alias mist="mvn -T 1C install -Dmaven.test.skip=true"                   # maven install skip test execution and compilation
+alias mfi="mist -nsu -o"                                                # maven install skip tests offline
+alias mcfi="mvn -T 1C clean install -Dmaven.test.skip=true -nsu -o"     # maven clean install skip tests offline
