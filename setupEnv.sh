06bd9578149147e57934a0c3c1a3a6dae84c106d
Samuel Campos <samuelcampos@users.noreply.github.com>
2025-07-29 21:56:44 +0100

    Update

--- setupEnv.sh
+++ setupEnv.sh
@@ -51,7 +51,7 @@ function setupSSHConfig () {
 }
 
 function installZSH () {
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 }
 
 function setupBashProfile () {
